36364895d441ca7e9c968df434b545368c45f415
Brian Monaccio <brianmonaccio@gmail.com>
2020-06-07 18:00:39 -0400

    Implements the solution

--- warmup/jumping-on-clouds/jumpingOnClouds.spec.js
+++ warmup/jumping-on-clouds/jumpingOnClouds.spec.js
@@ -5,5 +5,6 @@ describe('jumpingOnClouds', () => {
     expect(jumpingOnClouds([0, 1, 0, 0, 0, 1, 0])).toEqual(3);
     expect(jumpingOnClouds([0, 0, 1, 0, 0, 1, 0])).toEqual(4);
     expect(jumpingOnClouds([0, 0, 0, 0, 1, 0])).toEqual(3);
+    expect(jumpingOnClouds([0, 0, 0, 1, 0, 0])).toEqual(3);
   });
 });
