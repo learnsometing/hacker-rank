12bf66ef955b6364b79fc1a24d75c7b4bf533100
Brian Monaccio <brianmonaccio@gmail.com>
2020-06-08 12:14:11 -0400

    Completes the 2d array solution and confirms test passes

--- arrays/2d-array/2d-array.js
+++ arrays/2d-array/2d-array.js
@@ -1,6 +1,15 @@
 // Complete the hourglassSum function below.
 function hourglassSum(arr) {
-  
+  var max = -63;
+  for (let row = 0; row < 4; row++){
+    for (let col = 0; col < 4; col++){
+      let sum = arr[row][col] + arr[row][col + 1] + arr[row][col + 2];
+      sum += arr[row + 1][col + 1];
+      sum += arr[row + 2][col] + arr[row + 2][col + 1] + arr[row + 2][col + 2];
+      if (sum > max) max = sum;
+    }
+  }
+  return max;
 }
 
 module.exports = hourglassSum;
