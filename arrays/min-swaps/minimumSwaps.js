dbd2c30618516a1aadeaef18b835e18bd151ac1b
Brian Monaccio <brianmonaccio@gmail.com>
2020-06-08 13:52:41 -0400

    Adds my min swaps solution

--- arrays/min-swaps/minimumSwaps.js
+++ arrays/min-swaps/minimumSwaps.js
@@ -1,6 +1,19 @@
 function minimumSwaps(arr) {
-
-
+  var minSwaps = 0;
+  do {
+    var swapped = false;
+    for (let i = 0; i < arr.length; i++){
+      let el = arr[i];
+      if (el !== i + 1){
+        let temp = arr[el - 1];
+        arr[el - 1] = el;
+        arr[i] = temp;
+        swapped = true;
+        minSwaps++;
+      }
+    }
+  } while(swapped);
+  return minSwaps;
 }
 
 module.exports = minimumSwaps;
