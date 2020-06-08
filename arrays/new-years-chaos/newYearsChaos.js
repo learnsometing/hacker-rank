f0c6ab8b1d829803d8386a9482b675e284b27079
Brian Monaccio <brianmonaccio@gmail.com>
2020-06-08 13:08:37 -0400

    Improves solution to new years chaos

--- arrays/new-years-chaos/newYearsChaos.js
+++ arrays/new-years-chaos/newYearsChaos.js
@@ -1,23 +1,15 @@
 function minimumBribes(q) {
   var minBribes = 0;
-  do {
-    var swapped = false;
-    for (let i = 0; i < q.length; i++) {
-      const numBribes = q[i] - (i + 1);
-      if (numBribes > 2){
-        console.log('Too chaotic');
-        return 'Too chaotic';
-      }
-
-      if (q[i] > q[i+1]) {
-        const temp = q[i];
-        q[i] = q[i+1];
-        q[i+1] = temp;
-        swapped = true;
-        minBribes++
-      }
+  for (let i = q.length - 1; i >= 0; i--){
+    const numBribes = q[i] - (i + 1);
+    if (numBribes > 2){
+      console.log('Too chaotic');
+      return 'Too chaotic';
     }
-  } while(swapped);
+    for (let j = 0; j < i; j++){
+        if (q[j] > q[i]) minBribes++;
+    }
+  }
 
   console.log(minBribes);
   return minBribes;
