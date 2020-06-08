cc40c29426d7d2be0eeba44cb37d5ba6681aeee2
Brian Monaccio <brianmonaccio@gmail.com>
2020-06-08 10:35:51 -0400

    Implements passing solution

--- warmup/repeatedString/repeatedString.js
+++ warmup/repeatedString/repeatedString.js
@@ -1,5 +1,17 @@
 function repeatedString(s, n){
-
+  // calculate the number of times the matches will be repeated
+  // and the fraction of s that remains afterwards
+  const length = s.length;
+  const numRepeats = Math.floor(n / length);
+  const remainder = n % length;
+  
+  var numMatches = (s.split('a').length - 1) * numRepeats;
+  for (let i = 0; i < remainder; i++){
+    if (s.charAt(i) === 'a'){
+      numMatches++
+    }
+  }
+  return numMatches;
 }
 
 module.exports = repeatedString;
